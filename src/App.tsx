681424b19eee95310f076cc7995b7e158cb3c9c4
andreabastidasc <andrea.bcolina@gmail.com>
2024-05-20 04:27:47 -0300

    app component fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,49 @@
-import React, { useState } from 'react';
+import { ChakraProvider } from '@chakra-ui/react'
+import { Provider } from 'react-redux';
+import { useState } from 'react';
+
 import { store } from './redux/store';
-import { Provider, useDispatch, useSelector } from 'react-redux';
 import Balance from './components/Balance/Balance';
 import SendBTC from './components/SendBtc/sendBtc';
 import TransactionHistory from './components/TransactionsHistory/transactionsHistory';
 
+
 const App: React.FC = () => {
     const [ showSendForm, setShowSendForm ] = useState(false);
-    const sendBTC = (address: string, amount: number, fee: number) => {
+
+    const sendTransfer = (address: string, amount: number, fee: number) => {
         store.dispatch({ type: 'SEND_BTC', payload: { address, amount, fee } });
+        const transactions = store.getState().transactions;
+        return transactions[0]?.status;
     };
 
     const balance = store.getState().balance;
 
     return (
         <Provider store={store}>
-            <div className="App">
-                <h1>BTC Simulaor</h1>
-                <Balance balance={balance} sendAction={setShowSendForm} />
-                {showSendForm && (
-                   <div>
-                        <SendBTC sendBTC={sendBTC} />
-                        <button onClick={() => setShowSendForm(false)}>
-                            Cancelar envío
-                        </button>
-                   </div>
-                )}
-                <TransactionHistory />
-            </div>
+            <ChakraProvider>
+                <div className="App">
+                    <h1>BTC Simulator</h1>
+                    <Balance
+                        balance={balance}
+                        sendAction={setShowSendForm}
+                    />
+                    {showSendForm && (
+                        <div>
+                            <SendBTC
+                                sendBTC={sendTransfer}
+                                showForm={setShowSendForm}
+                            />
+                            <button
+                                onClick={() => setShowSendForm(false)}
+                            >
+                                Cancelar envío
+                            </button>
+                        </div>
+                    )}
+                    <TransactionHistory />
+                </div>
+            </ChakraProvider>
         </Provider>
   );
 };
