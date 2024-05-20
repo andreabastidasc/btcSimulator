598c2713f05d87e3d7ff973ba5402c1abccd495f
andreabastidasc <andrea.bcolina@gmail.com>
2024-05-20 04:27:47 -0300

    add status toast

--- src/components/SendBtc/sendBtc.tsx
+++ src/components/SendBtc/sendBtc.tsx
@@ -1,17 +1,34 @@
-import React, { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useToast } from '@chakra-ui/react';
 
 interface SendBTCProps {
-    sendBTC: (address: string, amount: number, fee: number) => void;
+    sendBTC: (address: string, amount: number, fee: number) => 'success' | 'error';
+    showForm: (value: boolean) => void;
 }
 
-const SendBTC: React.FC<SendBTCProps> = ({ sendBTC }) => {
+const SendBTC: React.FC<SendBTCProps> = ({ sendBTC, showForm }) => {
     const [address, setAddress] = useState<string>('');
     const [amount, setAmount] = useState<string>('');
-    const fee = parseFloat((Math.random() * (0.0002 - 0.0001) + 0.0001).toFixed(5));
+    const [fee, setFee] = useState<number>(0);
+    const toast = useToast()
+
+    useEffect(() => {
+        const calculatedFee = parseFloat((Math.random() * (0.0002 - 0.0001) + 0.0001).toFixed(5));
+        setFee(calculatedFee);
+    }, []);
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
-        sendBTC(address, parseFloat(amount), fee);
+        const status = sendBTC(address, parseFloat(amount), fee);
+        showForm(false);
+        toast({
+            title: status === 'success' ? 'Transacción exitosa' : 'Transacción fallida',
+            description: status === 'success' ?  'Tu transferencia fue exitosa!' : 'Tuvimos un error al enviar tu dinero',
+            status: status,
+            duration: 9000,
+            isClosable: true,
+            position: 'top-right'
+        });
     };
 
     return (
@@ -19,13 +36,23 @@ const SendBTC: React.FC<SendBTCProps> = ({ sendBTC }) => {
             <h2>Send BTC</h2>
             <label>
                 Address:
-                <input type="text" value={address} onChange={(e) => setAddress(e.target.value)} required />
+                <input
+                    onChange={(e) => setAddress(e.target.value)}
+                    required
+                    type="text"
+                    value={address}
+                />
             </label>
             <label>
                 Amount:
-                <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} required />
+                <input
+                    onChange={(e) => setAmount(e.target.value)}
+                    required
+                    type="number"
+                    value={amount}
+                />
             </label>
-            <p>Network Fee: {fee} BTC</p>
+            <p>Comisión: {fee} BTC</p>
             <button type="submit">Send</button>
         </form>
     );
