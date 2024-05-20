07dead1f46995b03cd0a699a8d85c09c6e1ca31a
andreabastidasc <andrea.bcolina@gmail.com>
2024-05-20 04:27:46 -0300

    add text and button to balance

--- src/components/Balance/Balance.tsx
+++ src/components/Balance/Balance.tsx
@@ -1,20 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import { getExchangeRate } from '../../services/RatesService/rates.service';
 
 interface BalanceProps {
     balance: number;
+    sendAction: (value: boolean) => void;
 }
 
-const Balance: React.FC<BalanceProps> = ({ balance }) => {
+const Balance: React.FC<BalanceProps> = ({ balance, sendAction }) => {
     const [exchangeRate, setExchangeRate] = useState<number | null>(null);
 
     useEffect(() => {
         const fetchExchangeRate = async () => {
             try {
                 const rate = await getExchangeRate();
-
                 const parsedRate = parseInt(rate.rates.ARS_BUY)
+
+                console.log(rate)
                 setExchangeRate(parsedRate)
             } catch (error) {
                 console.error('Error fetching exchange rate', error);
@@ -24,13 +25,16 @@ const Balance: React.FC<BalanceProps> = ({ balance }) => {
         fetchExchangeRate();
     }, []);
 
-    const balanceInARS = exchangeRate ? (balance * exchangeRate).toFixed(2) : 'Loading...';
+    const getBalanceInARS = (balance: number) => {
+        return exchangeRate ? (balance * exchangeRate).toFixed(2) : '---';
+    }
 
     return (
         <div>
             <h2>Balance</h2>
             <p>{balance} BTC</p>
-            <p>ARS Equivalent: {balanceInARS}</p>
+            <p>Total equivalente en ARS: {getBalanceInARS(balance)}</p>
+            <button onClick={() => sendAction(true)}>Enviar</button>
         </div>
     );
 };
